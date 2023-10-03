945901e225150372f1ca8257bf9a4f73dbae9d9e
udhay <udaysapkal135@gmail.com>
2023-10-03 14:47:16 +0530

    new file has been added

---
+++ add.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    double num1, num3, sum;
+
+    cout << "Ennnter the first number: ";
+    cin >> num1;
+
+    cout << "Enter the second number: ";
+    cin >> num3;
+
+    sum = num1 + num3;
+
+    cout << "The sum of " << num1 << " and " << num3 << " is: " << sum << endl;
+
+    return 0;
+}
